d6f6f403a390cce45434f8ccee0679fd4dbe7af8
Gabriel Turta <gturta@gmail.com>
2018-04-28 16:48:35 +0300

    reorganizare

--- nginx/nginx.sh
+++
@@ -1,9 +0,0 @@
-docker run -d \
-    --name nginx \
-    --network docker-net \
-    -v `pwd`/etc/nginx/sites.d/:/etc/nginx/sites.d \
-    -v /etc/letsencrypt:/etc/letsencrypt \
-    -p 80:80 -p 443:443 \
-    --restart unless-stopped \
-    gturta/nginx
-
