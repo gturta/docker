1cc59ef426e6ca52e323e5cd9d72e253beae3929
Gabriel Turta <gturta@gmail.com>
2018-05-06 18:06:37 +0300

    reworked smbd&nmbd in the same container based on s6 template

--- fileserver/smbd.sh
+++ fileserver/smbd.sh
@@ -4,4 +4,5 @@ docker run -d --name fileserver_smbd \
     -p "139:139/tcp" -p "445:445/tcp" \
     --mount source=/mnt/storage,target=/mnt/storage,type=bind,bind-propagation=slave \
     --restart unless-stopped \
+    --tty \
     gturta/smbd
