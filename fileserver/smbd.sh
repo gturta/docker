8552872005232b0e918cc916b12fa57895894ee0
Gabriel Turta <gturta@gmail.com>
2018-05-06 15:48:04 +0300

    looks like docker-compose bind propagation is not working

---
+++ fileserver/smbd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker run -d --name fileserver_smbd \
+    -p "139:139/tcp" -p "445:445/tcp" \
+    --mount source=/mnt/storage,target=/mnt/storage,type=bind,bind-propagation=slave \
+    --restart unless-stopped \
+    gturta/smbd
